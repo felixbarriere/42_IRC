6a8ff36fda32933fc2f607cc5faa79271ad3f08e
felixbarriere <felixbarriere@DESKTOP-ETDM7CT.localdomain>
2023-02-23 11:38:02 +0100

    WIP recv()

--- src/class/server.cpp
+++ src/class/server.cpp
@@ -12,7 +12,7 @@ Server::Server(char* portNumberMain, char *password) : portNumber(portNumberMain
 	// Initialiser le socket du serveur (a mettre dans la class server)
 	this->s_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (this->s_socket < 0)
-		SERVER_ERR("Error during socket creation.");
+		SERVER_ERR("Error during socket creation");
 	else
 		std::cout << "SOCKET CREATION OK" << std::endl;
 
@@ -24,13 +24,13 @@ Server::Server(char* portNumberMain, char *password) : portNumber(portNumberMain
 
 	// Lier l'adresse au socket du serveur
 	if (bind(this->s_socket, (struct sockaddr *) &this->s_address, sizeof(this->s_address)) < 0)
-        SERVER_ERR("Error during bind.");   // try above 1024: the first 1024 ports are 'special' and usually need super user privileges to use them
+        SERVER_ERR("Error during bind");   // try above 1024: the first 1024 ports are 'special' and usually need super user privileges to use them
 	else
 		std::cout << "BIND OK" << std::endl;
 
 	// Écouter les connexions entrantes
 	if (listen(this->s_socket, 5) < 0)				//  5: backlog définit une longueur maximale jusqu'à laquelle la file des connexions en attente pour sockfd peut croître.
-        SERVER_ERR("Error while listening.");
+        SERVER_ERR("Error while listening");
 	else 
 		std::cout << "LISTEN OK" << std::endl;
 
@@ -66,7 +66,7 @@ void	Server::acceptClient(void)
 		<< " port " << ntohs(client_address.sin_port) << std::endl << std::endl;
 
 	// instancier la classe Client
-	// utiliser new. Client *new_client est simplement la déclaration d'un pointeur de type Client, mais il ne crée pas d'objet Client ni n'alloue de mémoire pour un tel objet ==> comportement indéfini.
+	// utiliser new: "Client *new_client" est simplement la déclaration d'un pointeur de type Client, mais il ne crée pas d'objet Client ni n'alloue de mémoire pour un tel objet ==> comportement indéfini.
 	this->clients[client_socket] = new Client(client_socket, client_address);
 
 	// On ajoute le socket client à notre tableau de fds
@@ -81,41 +81,49 @@ void	Server::receiveRequest(int	client_socket)
 	char 	buffer[BUFFER_SIZE + 1];
 	int		res;
 	std::cout << "DEBUG ===> Receive request" << std::endl << std::endl;
-	// (void)client_socket;
 
-	std::cout << "DEBUG ===> client_socket before receive: " << client_socket << std::endl;
-	std::cout << "DEBUG ===> this->getUser(client_socket)->c_socket before receive: " << this->getUser(client_socket)->c_socket << std::endl;
-	
-	// creer une methode receiveRequest avec recv()
+//	memset(&this->s_address, 0, sizeof(this->s_address));
+
+	memset(buffer, 0, BUFFER_SIZE + 1);
 
 	// int recv(int client_socket, void* buffer, size_t len, int flags);
 	res = recv(client_socket , buffer, BUFFER_SIZE, 0);   // Ou on definit le BUFFER_SIZE? len?
 
+	std::string buffer2(buffer);
+
 	if (res < 0)
         SERVER_ERR("Error during receipt");
 	else if (res == 0)
-		std::cout << "DEBUG ===> recv == 0 : disconnet client"  << std::endl;
+		std::cout << "DEBUG ===> recv == 0 : disconnect client"  << std::endl;
 	else
 		std::cout << "DEBUG ===> recv > 0 : MESSAGE FROM CLIENT"  << std::endl << std::endl;
 
 	buffer[res] = '\0';
 
 	std::cout << "DEBUG ===> buffer:"  << std::endl;
-	for (int i; i < 10; i++)
-		std::cout << buffer[i];
+	for (size_t i = 0; i < buffer2.size(); i++)
+		std::cout << buffer2[i];
+	std::cout << std::endl;
+	
 
 
 }
 
-// void Server::init_pollfd_struct(std::vector<struct pollfd> &fds)
-// {
-// 	this->fds.clear();
-// 	this->fds.push_back(pollfd());			
-// 	this->fds.back().events = POLLIN;		
-// 	this->fds.back().revents = 0;
+void Server::init_pollfd_struct(void)
+{
+	// std::cout << "DEBUG ===> fds:" << fds.back().fd << std::endl << std::endl;
+	// std::cout << "DEBUG ===> this->fds:" << this->fds.back().fd << std::endl << std::endl;
 
-// 	//la 2eme partie de la fct est censee d'initialiser le reste de clients
-// }
+	// (void)fds;
+
+	this->fds.clear();
+	this->fds.push_back(pollfd());	
+	this->fds.back().fd = this->s_socket;
+	this->fds.back().events = POLLIN;		
+	this->fds.back().revents = 0;
+
+	//la 2eme partie de la fct est censee d'initialiser le reste de clients
+}
 
 void	Server::usePoll(void)
 {
@@ -127,25 +135,30 @@ void	Server::usePoll(void)
 	while(!serv_run)
 	{ 
 		// Préparer le vector de fd pour poll()
-		//init_pollfd_struct(fds);
+		// init_pollfd_struct(this->fds);
+		init_pollfd_struct();
 
-		this->fds.clear();
-		this->fds.push_back(pollfd());	// fds argument, which is an array of structures of the following form: 
-         //  struct pollfd {
-         //      int   fd;         /* file descriptor */
-         //      short events;     /* requested events */
-         //      short revents;    /* returned events */
-         //  };
-		// Cette structure définit un tableau de descripteurs de fichier ou de pointeurs de fichier.
-		this->fds.back().fd = this->s_socket;
-		this->fds.back().events = POLLIN;		// données en attente de lecture.
-		this->fds.back().revents = 0;
+		// this->fds.clear();
+		// this->fds.push_back(pollfd());	// fds argument, which is an array of structures of the following form: 
+
+		// // Cette structure définit un tableau de descripteurs de fichier ou de pointeurs de fichier.
+		// this->fds.back().fd = this->s_socket;
+		// this->fds.back().events = POLLIN;		// données en attente de lecture.
+		// this->fds.back().revents = 0;
+		
+		
+		
 		if (poll(fds.data(), this->fds.size(), this->timeout) < 0)	// ou &this->fds[0], mais fds.data() permet de boucler par la suite
 	        SERVER_ERR("Error Poll()");
 
 
+		std::cout << "DEBUG ===> BEFORE for loop:" << std::endl << std::endl;
+
+
 		for (size_t i = 0; i < this->fds.size(); i++)
 		{
+			std::cout << "DEBUG ===> this->fds.size():" << this->fds.size() << std::endl << std::endl;
+			
 			if (this->fds[i].revents != 0) //revent == 1 : on a recu une requete de irssi
 			{
 				// mettre "this->fds[i].revents & POLLIN" car revents peut contenir plusieurs resultats en meme temps (en plus) que POLLIN
@@ -155,7 +168,7 @@ void	Server::usePoll(void)
 					{
 						this->acceptClient();
 					}
-					else if (i != 0)
+					else
 					{
 						this->receiveRequest(fds[i].fd);  //	on envoie le socket du dernier client ajouté: fds[i].fd (qu'on a défini dans acceptClient : this->fds.back().fd = client_socket;)
 						// OU this->clients[i.fd]: les clés dans la map Clients correspondent aux fd: on cherche le fd de l'index i (necessite que ce soit un iterator).
@@ -164,8 +177,8 @@ void	Server::usePoll(void)
 			}
 		}
 
-		std::cout << std::endl << " Server running (sleep: 1)" << std::endl;
-		sleep(1);
+		// std::cout << std::endl << " Server running (sleep: 1)" << std::endl;
+		// sleep(1);
 	}
 	
 	// If revents is not POLLIN, it's an unexpected result, log and end the server.          
@@ -195,7 +208,5 @@ Client*						Server::getUser(int fd)
 	return ret->second;
 
 }
-
-
 
 void	Server::setPortNumber(char * portNumber) {	this->portNumber = portNumber;	}
