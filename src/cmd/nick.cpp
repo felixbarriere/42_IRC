80ffa2e23eb872dfc43b0a9f738e75fdc07878a8
felixbarriere <felixbarriere@DESKTOP-ETDM7CT.localdomain>
2023-03-07 12:12:50 +0100

    WIP function NICK

--- src/cmd/nick.cpp
+++ src/cmd/nick.cpp
@@ -3,6 +3,9 @@
 void	nick(Server *server, Client *client) {
 	(void) server;
 	std::cout << "DEBUG ===> NICK function called"  << std::endl << std::endl;
+
+	// check command syntax
+
 	std::cout << "DEBUG ===> params[0]: " << client->getMessage()->getParams()[0] << std::endl << std::endl;
 	client->setNick(client->getMessage()->getParams()[0]);
 	std::cout << "DEBUG ===> client->getNick(): " << client->getNick() << std::endl << std::endl;
