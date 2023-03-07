f58b0c27203cff367e6aeb1f01695cea3e3abe91
felixbarriere <felixbarriere@DESKTOP-ETDM7CT.localdomain>
2023-03-07 12:12:13 +0100

    WIP function NICK

--- src/cmd/nick.cpp
+++ src/cmd/nick.cpp
@@ -3,5 +3,10 @@
 void	nick(Server *server, Client *client) {
 	(void) server;
 	std::cout << "DEBUG ===> NICK function called"  << std::endl << std::endl;
+	std::cout << "DEBUG ===> params[0]: " << client->getMessage()->getParams()[0] << std::endl << std::endl;
 	client->setNick(client->getMessage()->getParams()[0]);
+	std::cout << "DEBUG ===> client->getNick(): " << client->getNick() << std::endl << std::endl;
+
+	// utiliser send pour indiquer au client que le nickname à changé 
+
 }
