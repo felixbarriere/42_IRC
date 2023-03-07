2d08217f493b2420a57714f09e67a8c02c0e3499
OBeeNya <benjamin.aubignat@gmail.com>
2023-03-07 16:23:35 +0100

    deleted reply

--- src/cmd/join.cpp
+++ src/cmd/join.cpp
@@ -3,7 +3,7 @@
 void	join(Server *server, Client *client) {
 	(void) server;
 	if (!client->getMessage()->getParams().size())
-		client->sendMsg(ERR_NEEDMOREPARAMS + client->getNick() + ": err need more params")
+		client->sendMsg(ERR_NEEDMOREPARAMS + client->getNick() + ": err need more params");
 	// std::map<std::string, Channel>::iterator	it = server->getChannels().find(client->getMessage()->getParams()[0]);
 	// if (it == server->getChannels().end()) {
 	// 	if (client->getChannel())
