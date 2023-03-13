baed655a5d0cbc4fc5f3d7d055d487de9478233d
felixbarriere <felixbarriere@DESKTOP-ETDM7CT.localdomain>
2023-03-13 19:22:16 +0100

    start mode command

--- src/cmd/mode.cpp
+++ src/cmd/mode.cpp
@@ -3,4 +3,37 @@
 void	mode(Server *server, Client *client) {
 	(void)server;
 	(void)client;
+
+	// user Mode
+
+	// channel Mode
+
 }
+
+/* 
+		std::map<char, bool>				_modes;
+ */
+
+/* Command: MODE
+  Parameters: <target> [<modestring> [<mode arguments>...]] */
+
+/* USER MODES :
+- a : away
+- i : invisible
+- w : wallops
+- r : restricted
+- o : operator
+
+CHANNEL MODES :
+for users :
+    - O : channel creator
+    - o : channel operator
+    - v : voice
+for channels :
+    - i : invite only
+    - m : moderated
+    - n : outside privmsg disabled
+    - p : private
+    - t : topic locked
+    - k : key locked
+    - l : user limit */
