4cfefc509f3e64688d91ab9e781ffb2eded314df
Felix BARRIERE-CONSTANTIN <fbarrier@e2r4p14.clusters.42paris.fr>
2023-03-28 13:20:39 +0200

    small changes in quit 2

--- src/cmd/quit.cpp
+++ src/cmd/quit.cpp
@@ -9,13 +9,14 @@ void	quit(Server* server, Client* client) {
 	std::map<std::string, Channel>::iterator	it = server->getChannels().begin();
 	while (it != server->getChannels().end()) {
 		if (client->checkChannelName(it->first)) {		//Client is part of an existing Channel
-			it->second.broadcast(client, client->getNick() + str);
+			it->second.broadcast(client, "QUIT " + it->first + str);
 			client->removeChannelName(it->first);
 			it->second.removeMember(client);  // a bien tester
 
-			if (it->second.getMembers().size() == 0)
+			if (it->second.getMembers().size() == 0) {
 				// std::cout << "Client is creator of this channel" << std::endl;
 				server->getChannels().erase(it++);
+			}
 			else
 				it++;
 		}
