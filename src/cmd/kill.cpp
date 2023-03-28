40f17affe21daa7790e0718a9ea9b5c71f208512
Maryna SAMOILENKO <masamoil@e2r4p16.clusters.42paris.fr>
2023-03-28 15:28:02 +0200

    kick_n_kill

--- src/cmd/kill.cpp
+++ src/cmd/kill.cpp
@@ -4,25 +4,8 @@
      Command: KILL
   Parameters: <nickname> <comment>
 
-The KILL command is used to close the connection between a given client and the server they are connected to.
-KILL is a privileged command and is available only to IRC Operators. <nickname> represents the user to be ‘killed’,
-and <comment> is shown to all users and to the user themselves upon being killed.
-When a KILL command is used, the client being killed receives the KILL message,
-and the <source> of the message SHOULD be the operator who performed the command.
-The user being killed and every user sharing a channel with them receives a QUIT
-message representing that they are leaving the network. The <reason> on this QUIT 
-message typically has the form: "Killed (<killer> (<reason>))" where <killer> is 
-he nickname of the user who performed the KILL. The user being killed then
-receives the ERROR message, typically containing a <reason> of "Closing Link:
-<servername> (Killed (<killer> (<reason>)))". After this, their connection is closed.
-If a KILL message is received by a client, it means that the user specified by
-<nickname> is being killed. With certain servers, users may elect to receive KILL
-messages created for other users to keep an eye on the network. This behavior may also be restricted to operators.
-Clients can rejoin instantly after this command is performed on them.
-If the operator tries to remove a remote user but is not privileged to, 
-they should receive the ERR_NOPRIVS (723) numeric.
-<comment> SHOULD reflect why the KILL was performed. 
-For user-generated KILLs, it is up to the user to provide an adequate reason.
+https://www.tech-invite.com/y25/tinv-ietf-rfc-2812-2.html#e-3-7-1
+https://modern.ircdocs.horse/#kill-message
 
 Numeric Replies:
 ERR_NOSUCHSERVER (402)
@@ -45,20 +28,27 @@ void ft_kill(Server* server, Client* client) {
     //else if(checkMode(client) == false)
     else if (!client->getModes().find('o')->second)
             client->sendMsg(ERR_NOPRIVILEGES + client->getNick() +  " :Permission Denied- You're not an IRC operator", client);
-    else
-    {
+    else{
         size_t i = client->getMessage()->getParams()[1].find(":");
         std::string     comment = "";
         if (i != std::string::npos)
             comment = client->getMessage()->getParams()[1].substr(i + 1);
-        //std::string comment = client->getMessage()->getParams()[1];
         std::cout << "CHECK===> comment =  " << comment << std::endl;
         std::string str_to_all = "Killed ( " + client->getNick() + " " + comment + " )";
-        std::string str_to_user = "Closing Link: " +  client->getHostname() + " (Killed ( " + client->getNick() + " " + comment + " )";
-                //send the KILL message to user killed
-                server->getClientByNick(client->getMessage()->getParams()[0])->sendMsg(str_to_user, server->getClientByNick(client->getMessage()->getParams()[0]));
+        std::string str_to_user = "Closing Link: " +  client->getHostname() + " (Killed (" + client->getNick() + " (" + comment + ")))";
+		//send the KILL message to user killed
+		server->getClientByNick(client->getMessage()->getParams()[0])->sendMsg(str_to_user, server->getClientByNick(client->getMessage()->getParams()[0]));
         //send the QUIT msg to everyone in the channel - in cmd quit->broadcast
         quit(server, server->getClientByNick(client->getMessage()->getParams()[0]));
-                //kick(server, server->getUserbyNick(client->getMessage()->getParams()[0]));
+		std::map<std::string, Channel>::iterator	it = server->getChannels().begin();
+		while (it != server->getChannels().end()) {
+			if (client->checkChannelName(it->first)) {
+				it->second.broadcast(client, str_to_all);
+			}
+			++it;
+		}
+
+		//server->(client, str_to_all);
+		server->removeClient(server->getClientByNick(client->getMessage()->getParams()[0]), server->getClientByNick(client->getMessage()->getParams()[0])->getC_socket());
     }
 }
